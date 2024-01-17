bab9e45aea025c40c984940f84c2efd851e98889
sharonyang05 <141669628+sharonyang05@users.noreply.github.com>
2024-01-17 13:48:26 -0800

    Add files via upload

---
+++ SearchEngine.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> stringList = new ArrayList<>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                stringList.add(parameters[1]);
+                return String.format("'%s' added.", parameters[1]);
+            }
+        } else if (url.getPath().contains("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                String results = "";
+                for (String str: stringList) {
+                    if (str.contains(parameters[1])) {
+                        results = results + str + ", ";
+                    }
+                }
+                if (results.length() > 0) {
+                    results = results.substring(0, results.length()-2);
+                    return results;
+                } else {
+                    return "No results found.";
+                }
+            }
+        }
+        return "404 Not Found!";
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        Server.start(port, new Handler());
+    }
+}
